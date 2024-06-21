1afaf38b2aed778751a5fa7d477de30c57bd6b21
Felix Gahler <felixbenjamin.gahler@student.hs-anhalt.de>
2024-06-21 18:16:04 +0200

    Hello World

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld/>
 </template>
 
 <script>
@@ -20,7 +19,11 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  background: white;
 }
 </style>
